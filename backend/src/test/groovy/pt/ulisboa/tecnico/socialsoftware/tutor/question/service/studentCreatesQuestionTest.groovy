dc5cf2a829b2b4143a702da33abe93319523928f
André Nobre <andre.nobre@tecnico.ulisboa.pt>
2020-03-12 11:49:28 +0000

    ISSUE #2 Closed F1.1 Create a new question - define the test cases

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/studentCreatesQuestionTest.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+
+class studentCreatesQuestionTest extends Specification{
+
+    def 'student is enrolled in the course'(){
+        expect:false
+    }
+
+    def 'student creates a question with one option and no image'(){
+        expect: false
+    }
+
+    def 'student creates a question with an image and one option'(){
+        expect: false
+    }
+
+    def 'student creates a question with multiple options and no image'(){
+        expect: false
+    }
+
+
+    
+}
